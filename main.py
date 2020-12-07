a0881caa9a1af53ae82450d129f7e78b12343dae
adamdhall <adamdhall@outlook.com>
2020-12-07 20:53:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+   if char in alphabet:
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    position = alphabet.index(char)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  else:
+    end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+def run_again(choice):
+  if choice =="Y":
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  choice = input("Do you want to run the cipher again? (Y/N)")
+
+  if shift > 25:
+    shift = shift % 25
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  run_again(choice)
